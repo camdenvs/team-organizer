a45ce56f6637e00c49dd3e3ff3f5500e3f0eae29
Camden Van Sciver <camdenvs02@gmail.com>
2022-11-18 11:54:51 -0700

    added all necessary classes

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,13 +1,15 @@
+const Manager = require('../lib/Manager')
+
 describe('Manager subclass', () => {
     it('Takes an extra parameter for officeNumber', () => {
         const manager = new Manager('Terry', '2', 't3rry@fakemail.com', '1')
 
-        expect(manager.officeNumber).toEqual('1')
+        expect(manager.officeNumber).toBe('1')
     })
     
     it('return "Manager" as the role', () => {
         const manager = new Manager('Terry', '2', 't3rry@fakemail.com', '1')
 
-        expect(manager.getRole().toEqual('Manager'))
+        expect(manager.getRole()).toBe('Manager')
     })
 })
